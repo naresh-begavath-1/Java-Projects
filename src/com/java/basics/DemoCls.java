8ae43fb5e241ebd511eea4312ff12c34823c4a35
Naresh Begavath <bnaresh530@gmail.com>
2021-05-16 20:24:33 +0530

    Initial commit

---
+++ src/com/java/basics/DemoCls.java
@@ -0,0 +1,12 @@
+package com.java.basics;
+
+public class DemoCls {
+    public static void main(String[] args) {
+        // Sample print statement
+        System.out.println("Hello World");
+        // Creating an object for Products class
+        Products products = new Products("Keyboard");
+        // Print the created object
+        System.out.println(products.getName());
+    }
+}
---
+++ src/com/java/basics/Products.java
@@ -0,0 +1,13 @@
+package com.java.basics;
+
+public class Products {
+    // Variable declaration
+    private String name;
+    // Method declaration
+    public Products (String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
